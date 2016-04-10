5998716e4dbf46eaff0160b9f1e505f5a9e1c4c9
Unknown W. Brackets <checkins@unknownbrackets.org>
2016-04-10 15:56:06 -0700

    Correct some bugs parsing version numbers.
    Especially stable version numbers.

---
+++ test/version.js
@@ -0,0 +1,133 @@
+var vows = require('vows');
+var assert = require('assert');
+var lookups = require('../lib/lookups');
+
+vows.describe('Version parsing').addBatch({
+	'A major stable version': {
+		topic: lookups.calcVersionValue('v2.0'),
+
+		'should parse': function (value) {
+			assert.notEqual(value, 0);
+		},
+
+		'should be greater than last stable': function (value) {
+			assert.isTrue(value > lookups.calcVersionValue('v1.2.2'));
+		},
+
+		'should be less than next stable': function (value) {
+			assert.isTrue(value < lookups.calcVersionValue('v3.0'));
+		},
+	},
+
+	'A minor stable version': {
+		topic: lookups.calcVersionValue('v1.3'),
+
+		'should parse': function (value) {
+			assert.notEqual(value, 0);
+		},
+
+		'should be greater than last stable': function (value) {
+			assert.isTrue(value > lookups.calcVersionValue('v1.2.2'));
+		},
+
+		'should be less than next stable': function (value) {
+			assert.isTrue(value < lookups.calcVersionValue('v1.3.1'));
+		},
+	},
+
+	'A patch stable version': {
+		topic: lookups.calcVersionValue('v1.2.2'),
+
+		'should parse': function (value) {
+			assert.notEqual(value, 0);
+		},
+
+		'should be greater than last stable': function (value) {
+			assert.isTrue(value > lookups.calcVersionValue('v1.2.1'));
+		},
+
+		'should be less than next stable': function (value) {
+			assert.isTrue(value < lookups.calcVersionValue('v1.3.0'));
+		},
+	},
+
+	'A revision stable version': {
+		topic: lookups.calcVersionValue('v1.2.2.1'),
+
+		'should parse': function (value) {
+			assert.notEqual(value, 0);
+		},
+
+		'should be greater than last stable': function (value) {
+			assert.isTrue(value > lookups.calcVersionValue('v1.2.2'));
+		},
+
+		'should be less than next stable': function (value) {
+			assert.isTrue(value < lookups.calcVersionValue('v1.2.2.2'));
+		},
+	},
+
+	'An unstable major version': {
+		topic: lookups.calcVersionValue('v2.0-286-gabcdef0'),
+
+		'should parse': function (value) {
+			assert.notEqual(value, 0);
+		},
+
+		'should be greater than last stable': function (value) {
+			assert.isTrue(value > lookups.calcVersionValue('v2.0'));
+		},
+
+		'should be less than next stable': function (value) {
+			assert.isTrue(value < lookups.calcVersionValue('v2.1'));
+		},
+	},
+
+	'An unstable minor version': {
+		topic: lookups.calcVersionValue('v1.2-286-gabcdef0'),
+
+		'should parse': function (value) {
+			assert.notEqual(value, 0);
+		},
+
+		'should be greater than last stable': function (value) {
+			assert.isTrue(value > lookups.calcVersionValue('v1.2'));
+		},
+
+		'should be less than next stable': function (value) {
+			assert.isTrue(value < lookups.calcVersionValue('v1.2.1'));
+		},
+	},
+
+	'An unstable patch version': {
+		topic: lookups.calcVersionValue('v1.2.2-286-gabcdef0'),
+
+		'should parse': function (value) {
+			assert.notEqual(value, 0);
+		},
+
+		'should be greater than last stable': function (value) {
+			assert.isTrue(value > lookups.calcVersionValue('v1.2.2'));
+		},
+
+		'should be less than next stable': function (value) {
+			assert.isTrue(value < lookups.calcVersionValue('v1.2.3'));
+		},
+	},
+
+	'An unstable revision version': {
+		topic: lookups.calcVersionValue('v1.2.2.1-286-gabcdef0'),
+
+		'should parse': function (value) {
+			assert.notEqual(value, 0);
+		},
+
+		'should be greater than last stable': function (value) {
+			assert.isTrue(value > lookups.calcVersionValue('v1.2.2.1'));
+		},
+
+		'should be less than next stable': function (value) {
+			assert.isTrue(value < lookups.calcVersionValue('v1.2.3'));
+		},
+	},
+}).export(module);
